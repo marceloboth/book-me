b481d558a3383122b38c7188a3a44d0ccb0c27c3
Marcelo J. Both <marcelo.both@gmail.com>
2017-10-04 08:24:57 -0300

    Add mirage configuration

--- config/environment.js
+++ config/environment.js
@@ -24,6 +24,20 @@ module.exports = function(environment) {
     }
   };
 
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'script-src': "'self' 'unsafe-inline'",
+    'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
+    'font-src': "'self' fonts.gstatic.com",
+    'connect-src': "'self'",
+    'img-src': "'self' data:",
+    'media-src': "'self'"
+  };
+
+  ENV['ember-cli-mirage'] = {
+    discoverEmberDataModels: true
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;

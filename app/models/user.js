9d269dcea9368708dc5b6c072e0985098f2caacb
Marcelo J. Both <marcelo.both@gmail.com>
2017-10-07 22:05:13 -0300

    Add pass confirmation

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,12 @@
 import DS from 'ember-data';
 
-const { Model, attr} = DS;
+const { 
+  Model, 
+  attr
+} = DS;
 
 export default Model.extend({
   email: attr('string'),
-  password: attr('string')
+  password: attr('string'),
+  passwordConfirmation: attr('string')
 });
